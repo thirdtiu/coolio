ac3536f6c5b517cdc9e5351fb1088c56abbfa530
Avelino Tiu <atiu@exist.com>
2015-12-02 13:41:24 +0800

    navbar bootstrap customisation

--- gruntfile.js
+++ gruntfile.js
@@ -42,10 +42,37 @@ module.exports = function(grunt) {
       }
     },
 
+    uglify: {
+      production: {
+        files: {
+          'prod/assets/js/scripts.min.js' : ['src/javascript/lib/jquery-1.11.3.js', 'src/bootstrap-sass-3.3.6/assets/javascripts/bootstrap.js', 'node_modules/angular/angular.js',  'src/javascript/custom/*.js']
+        }
+      },
+      development: {
+        options: {
+          beautify: true, 
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        files: {
+          'dev/assets/js/scripts.min.js' : ['src/javascript/lib/jquery-1.11.3.js', 'src/bootstrap-sass-3.3.6/assets/javascripts/bootstrap.js', 'node_modules/angular/angular.js', 'src/javascript/custom/*.js']
+        }
+      }
+    },
+
     watch: {
       css:{
         files: ['src/scss/*.scss', 'src/bootstrap-sass-3.3.6/assets/stylesheets/**/*.scss'],
         tasks: ['sass:development']
+      },
+      scripts:{
+        files: 'src/javascript/**/*.js',
+        tasks: ['uglify:development']
+      },
+      html:{
+        files: 'src/*.html',
+        tasks: ['htmlmin:development']
       }
     }
 
@@ -61,8 +88,10 @@ module.exports = function(grunt) {
 
 
 
-  // Default task(s).
-  grunt.registerTask('default', ['htmlmin:development', 'sass:development']);
-  grunt.registerTask('production', ['htmlmin:production', 'sass:production']);
+  // Default tasks.
+  grunt.registerTask('default', ['htmlmin:development', 'sass:development', 'uglify:development']);
+
+  // Production tasks.
+  grunt.registerTask('production', ['htmlmin:production', 'sass:production', 'uglify:production']);
 
 };
